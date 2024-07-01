1b3ad0b26cae770c02a2850c7be73a162da2ff34
chrisalvative <chris-alvative@paystack.com>
2024-07-01 08:33:53 +0100

    clean up

--- index.js
+++ index.js
@@ -6,18 +6,19 @@ const PORT = 3000;
 
 app.get('/api/hello', async (req, res) => {
     const visitorName = req.query.visitor_name || 'Guest';
-    // Get the location based on IP address
-    const clientIp = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    // Get IP address
+    const clientIp = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
     
     try {
         // get city, long and lat using IP address
-        const locationResponse = await axios.get(`https://ipapi.co/${clientIp}/json/`);
+        const locationResponse = await axios.get(`https://ipapi.co/102.88.36.188/json/`);
         const city = locationResponse.data.city || 'Unknown location';
         const longitude = locationResponse.data.longitude
         const latitude = locationResponse.data.latitude
+
         // get weather information using long and lat
-        const weatherResponse = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=-${longitude}&appid=${process.env.WEATHER_KEY}&units=metric`)
-        const temperature = weatherResponse.data.main.temp;
+        const weatherResponse = await axios.get(`https://api.weatherapi.com/v1/current.json?key=${process.env.WEATHER_KEY}&q=${latitude + "," + longitude}`)
+        const temperature = weatherResponse.data.current.temp_c;
         
         const response = {
             client_ip: clientIp,
